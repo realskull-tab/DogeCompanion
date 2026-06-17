c510d76cc7fcc26b6b3276d2a91d610d0a1b6c2e
realskull-tab <bonne@realskull>
2026-06-17 18:35:04 +1000

    Minor Changes

--- src/main/java/net/mcreator/dogecompanion/item/PremiumKibbleItem.java
+++ src/main/java/net/mcreator/dogecompanion/item/PremiumKibbleItem.java
@@ -1,11 +1,21 @@
 package net.mcreator.dogecompanion.item;
 
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.entity.player.Player;
+
+import net.mcreator.dogecompanion.procedures.PremiumKibbleItemIsCraftedsmeltedProcedure;
 
 public class PremiumKibbleItem extends Item {
 	public PremiumKibbleItem(Item.Properties properties) {
 		super(properties.rarity(Rarity.UNCOMMON).stacksTo(16).food((new FoodProperties.Builder()).nutrition(2).saturationModifier(0.3f).build()));
+	}
+
+	@Override
+	public void onCraftedBy(ItemStack itemstack, Player entity) {
+		super.onCraftedBy(itemstack, entity);
+		PremiumKibbleItemIsCraftedsmeltedProcedure.execute(entity);
 	}
 }
---
+++ src/main/java/net/mcreator/dogecompanion/procedures/PremiumKibbleItemIsCraftedsmeltedProcedure.java
@@ -0,0 +1,25 @@
+package net.mcreator.dogecompanion.procedures;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.AdvancementHolder;
+
+public class PremiumKibbleItemIsCraftedsmeltedProcedure {
+	public static void execute(Entity entity) {
+		if (entity == null)
+			return;
+		if (entity instanceof ServerPlayer _player && _player.level() instanceof ServerLevel _level) {
+			AdvancementHolder _adv = _level.getServer().getAdvancements().get(ResourceLocation.parse("dogecompanion:very_tasty"));
+			if (_adv != null) {
+				AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
+				if (!_ap.isDone()) {
+					for (String criteria : _ap.getRemainingCriteria())
+						_player.getAdvancements().award(_adv, criteria);
+				}
+			}
+		}
+	}
+}
